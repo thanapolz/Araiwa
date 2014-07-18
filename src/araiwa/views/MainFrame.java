6f3a19261d54df6b22360071504bd92d1a19e7d0
Administrator <Administrator@10.240.19.40>
2014-07-18 14:17:56 +0700

    add main frame

--- src/araiwa/views/MainFrame.java
+++ src/araiwa/views/MainFrame.java
@@ -29,6 +29,7 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("ระบบจัดการสายการบิน อย่างง่าย");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
